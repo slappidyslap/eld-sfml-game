d34acd45f1baf7895383871a3cfb153b5b63ffa6
Eldiyar <musabaeveldiar@gmail.com>
2025-12-24 17:07:44 +0600

    RectangleShape, Texture, Sprite

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,14 @@
 
 int main()
 {
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
+    auto window = sf::RenderWindow(sf::VideoMode({640, 480}), "CMake SFML Project");
     window.setFramerateLimit(60);
-    
-    if (window.isOpen()) {
+
+    if (window.isOpen())
+    {
         dgrdch::logInfo("App started");
     }
-    
+
     while (window.isOpen())
     {
         sf::Event event;
@@ -21,8 +22,22 @@ int main()
                 window.close();
             }
         }
-
         window.clear(sf::Color::Red);
+
+        sf::RectangleShape rectangle(sf::Vector2f(128.0f, 128.0f));
+        rectangle.setFillColor(sf::Color::Blue);
+        rectangle.setPosition(320, 240);
+        rectangle.setOrigin(64, 64);
+        window.draw(rectangle);
+
+        sf::Texture worldTilesetTexture;
+        if (!worldTilesetTexture.loadFromFile("world_tileset.png"))
+        {
+            dgrdch::logError("Unable to load world_tileset.png");
+            exit(1);
+        }
+        sf::Sprite worldTileset{worldTilesetTexture};
+        window.draw(worldTileset);
 
         window.display();
     }
